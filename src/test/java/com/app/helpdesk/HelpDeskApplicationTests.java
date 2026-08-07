e9f14d2621086c4979e84e4ce5d01a81e81cb138
wkrrql <189556823+wkrrql@users.noreply.github.com>
2026-08-07 12:22:41 +0300

    start new project:)

---
+++ src/test/java/com/app/helpdesk/HelpDeskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.helpdesk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelpDeskApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
